0aa901824b279f4979fa97df0f03779518a65f60
Abel Castro <hi@abelcastro.dev>
2023-10-02 10:25:31 +0200

    Fix api app_name

--- app/api/urls.py
+++ app/api/urls.py
@@ -1,7 +1,8 @@
 from api.views import PostListAPI
 from django.urls import path
 
-app_name = "users"
+app_name = "api"
+
 urlpatterns = [
     path("posts/", view=PostListAPI.as_view(), name="post_list"),
 ]
