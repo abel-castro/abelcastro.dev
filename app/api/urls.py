4db26af8e03724de1e23adc5383990b6d648e87e
Abel Castro <hi@abelcastro.dev>
2023-10-06 00:20:32 +0200

    Add cache to the /api/posts

--- app/api/urls.py
+++ app/api/urls.py
@@ -1,8 +1,10 @@
 from api.views import PostListAPI
 from django.urls import path
+from django.views.decorators.cache import cache_page
+
 
 app_name = "api"
 
 urlpatterns = [
-    path("posts/", view=PostListAPI.as_view(), name="post_list"),
+    path("posts/", view=cache_page(60 * 60)(PostListAPI.as_view()), name="post_list"),
 ]
