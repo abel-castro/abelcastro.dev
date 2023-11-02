53df1e5e8bf3bdc9b30bc80855a3400133235645
Abel Castro <hi@abelcastro.dev>
2023-11-02 06:18:07 +0100

    Fix for displaying updated_at in the admin

--- app/sports/admin.py
+++ app/sports/admin.py
@@ -2,5 +2,9 @@ from django.contrib import admin
 from sports.models import League, Team
 
 
+class LeagueModelAdmin(admin.ModelAdmin):
+    readonly_fields = ('updated_at',)
+
+
+admin.site.register(League, LeagueModelAdmin)
 admin.site.register(Team)
-admin.site.register(League)
