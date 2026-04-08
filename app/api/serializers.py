e880a8b082f29c1492bbcea19918801432b1310a
Abel Castro <abel.castro@vivid-planet.com>
2026-04-08 21:55:49 +0200

    Migrate tags to lowercase

--- app/api/serializers.py
+++ app/api/serializers.py
@@ -7,6 +7,9 @@ class TagSerializer(serializers.ModelSerializer):
         model = Tag
         fields = ("name",)
 
+    def validate_name(self, value):
+        return value.lower()
+
 
 class PostSerializer(serializers.ModelSerializer):
     tags = TagSerializer(many=True, read_only=True)
