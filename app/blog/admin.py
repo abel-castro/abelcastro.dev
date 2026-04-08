16577ca925935056ec8477365410d6dfad7ebe0a
Abel Castro <hi@abelcastro.dev>
2026-04-08 22:06:06 +0200

    Add admin post filters

--- app/blog/admin.py
+++ app/blog/admin.py
@@ -2,5 +2,11 @@ from django.contrib import admin
 
 from .models import Post, Tag
 
-admin.site.register(Post)
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    search_fields = ["title", "content", "tags__name"]
+    list_filter = ["published", "tags", "date"]
+
+
 admin.site.register(Tag)
